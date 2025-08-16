0b7d09619bfd4a4eada14df03ed223e0f5597f6f
Santiago <santiago_abregu_@hotmail.com>
2025-08-16 13:15:39 -0300

    backend setup

---
+++ backend/src/main/java/com/beewise/BeeWiseApplication.java
@@ -0,0 +1,13 @@
+package com.beewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeWiseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/beewise/BeeWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
