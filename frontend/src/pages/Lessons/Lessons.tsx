4af0c60591ad3c50019c473aed8105295a7ea9de
Tobias Torres <tobias.torres777@gmail.com>
2025-08-22 01:51:05 -0300

    Agrego Routing

---
+++ frontend/src/pages/Lessons/Lessons.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MainLayout from "../../components/layout/MainLayout";
+
+export function LessonsPage() {
+  return (
+    <MainLayout title="Lessons Page">
+      <h1>Lessons Page</h1>
+    </MainLayout>
+  );
+}
