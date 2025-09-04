b29219aa2e17104b67e88c8a8bfd3ef7946928ee
Santiago <santiago_abregu_@hotmail.com>
2025-09-04 14:15:06 -0300

    Feat (front): Routes to Challenges & Ranking

---
+++ frontend/src/pages/Ranking/Ranking.tsx
@@ -0,0 +1,9 @@
+import MainLayout from "../../components/layout/MainLayout";
+
+export function RankingPage() {
+  return (
+    <MainLayout title="Perfil">
+      <h1>PROXIMAMENTE</h1>
+    </MainLayout>
+  );
+}
