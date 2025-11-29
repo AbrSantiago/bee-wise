ee94b306e06ea35a1f9ea1c16331437e95dfeef5
Tobias Torres <tobias.torres777@gmail.com>
2025-11-29 00:33:31 -0300

    Feat(Front): Fix in avatarEdit, ChallengePlay, RouletteScreen

--- frontend/src/pages/Challenges/RouletteScreen.tsx
+++ frontend/src/pages/Challenges/RouletteScreen.tsx
@@ -2,6 +2,7 @@ import CategoryPopup from "../../components/layout/CategoryPopup";
 import Roulette from "../../components/layout/Roulette";
 import type { ExerciseCategory } from "../../services/challengeService";
 import "./RouletteScreen.css";
+import beeIcon from "../../../image/BeeCoin.png";
 
 interface Props {
   loading: boolean;
@@ -24,7 +25,32 @@ export default function RouletteScreen({
   onSpinEnd,
   onStartGame,
 }: Props) {
-  if (loading) return;
+  if (loading) {
+    return (
+      <div className="roulette-container">
+        <div className="loading-message">
+          <div className="spinner-wrapper">
+            {/* Anillos decorativos giratorios */}
+            <div className="spinner-ring outer"></div>
+            <div className="spinner-ring inner"></div>
+            {/* El icono central fijo pero palpitando */}
+            <div className="spinner-icon">
+              <img src={beeIcon} alt="Loading bee" />
+            </div>
+          </div>
+
+          <div className="text-content">
+            <h2>Preparando el desafío</h2>
+            <p>Seleccionando categorías...</p>
+          </div>
+
+          <div className="loading-bar">
+            <div className="loading-bar-progress"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="roulette-container">
